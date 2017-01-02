09d8403716d4c2c441c89cf9cfa4a75f0c80840b
jirishus <jirishus@yahoo.com>
2017-01-02 10:54:00 -0800

    google analytics

--- header.php
+++ header.php
@@ -1,12 +1,22 @@
 <!DOCTYPE HTML>
 
 <head>
- <title><?php wp_title('|'); ?></title>
- <meta charset="<?php bloginfo( 'charset' ); ?>" />
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<title><?php wp_title('|'); ?></title>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
  
- <?php wp_head(); ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-39146802-2', 'auto');
+  ga('send', 'pageview');
+  ga('send', 'event', 'Video', 'play', 'some random label');
+</script>
+<?php wp_head(); ?>
 </head>
 <body>
 
